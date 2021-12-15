5d2e28802411f6ef27ecb25db0fa274b867ca52e
JasonMcKinlay <70662170+JasonMcKinlay@users.noreply.github.com>
2021-12-14 21:43:50 -0600

    Started DogCatNN

---
+++ DogCatNN.py
@@ -0,0 +1,142 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+import random
+import pickle
+import tensorflow as tf
+
+DATADIR = "D:\School Files\Software Engineering\kagglecatsanddogs_3367a\PetImages"
+CATEGORIES = ["Dog", "Cat"]
+
+for category in CATEGORIES:
+    path = os.path.join(DATADIR, category) # path to cats or dogs dir
+    for img in os.listdir(path):
+        img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
+        plt.imshow(img_array, cmap = "gray")
+        plt.show()
+        break
+    break
+
+# print(img_array)
+
+IMG_SIZE = 50
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap = "gray")
+plt.show()
+
+
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category) # path to cats or dogs dir
+        class_num = CATEGORIES.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array, class_num])
+            except Exception as e:
+                pass
+
+create_training_data()
+print(len(training_data))
+
+random.shuffle(training_data)
+
+for sample in training_data[:10]:
+    print(sample[1])
+
+x = []
+y = []
+
+for features, label in training_data:
+    x.append(features)
+    y.append(label)
+
+x = np.array(x).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+pickle_out = open("X.pickle", "wb")
+pickle.dump(x, pickle_out)
+pickle_out.close()
+
+pickle_out = open("X.pickle", "wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+# ---------------------------------------------------------------------------------
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+# more info on callbakcs: https://keras.io/callbacks/ model saver is cool too.
+from tensorflow.keras.callbacks import TensorBoard
+import pickle
+import time
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+dense_layers = [0]
+layer_sizes = [64]
+conv_layers = [3]
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            print(NAME)
+
+            model = Sequential()
+
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            model.add(Flatten())
+
+            for _ in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
+
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+
+            tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+            model.compile(loss='binary_crossentropy',
+                          optimizer='adam',
+                          metrics=['accuracy'],
+                          )
+
+            model.fit(X, y,
+                      batch_size=32,
+                      epochs=10,
+                      validation_split=0.3,
+                      callbacks=[tensorboard])
+
+model.save('64x3-CNN.model')
+
+def prepare(filepath):
+    IMG_SIZE = 70  # 50 in txt-based
+    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+model = tf.keras.models.load_model("64x3-CNN.model")
+
+prediction = model.predict([prepare('doggo.jpg')])
+print(prediction)  # will be a list in a list.
+print(CATEGORIES[int(prediction[0][0])])
+
